90da6a6401682768b615ebe4be82ab7e29c42840
kok wee <unlearnt92@gmail.com>
2024-06-12 14:04:20 +0800

    update

--- app/api/collect_shirt/route.ts
+++ app/api/collect_shirt/route.ts
@@ -1,6 +1,101 @@
 import {getSupabaseClient} from "@/app/service/supabase/supabase";
 
 export const dynamic = 'force-dynamic' // defaults to auto
+
+async function get_locker(shirt_size: String){
+
+    console.log("get locker selected shirt size ", shirt_size)
+
+    let map = new Map();
+
+    map.set("XS", {
+        quantity : 10,
+        lockers : [
+            { locker_no : "11-0247", locker_password: "1590"},
+        ]
+    })
+
+    map.set("S", {
+        quantity : 50,
+        lockers : [
+            { locker_no : "11-0246", locker_password: "5000"},
+        ]
+    })
+
+    map.set("M", {
+        quantity : 45,
+        lockers : [
+            { locker_no : "11-0248", locker_password: "2370"},
+            { locker_no : "11-0250", locker_password: "4100"}
+        ]
+    })
+
+    map.set("L", {
+        quantity : 70,
+        lockers : [
+            { locker_no : "11-0251", locker_password: "4472"},
+            { locker_no : "11-0245", locker_password: "8600"}
+        ]
+    })
+
+    map.set("XL", {
+        quantity : 50,
+        lockers : [
+            { locker_no : "11-0244", locker_password: "9296"},
+        ]
+    })
+
+    map.set("2XL", {
+        quantity : 16,
+        lockers : [
+            { locker_no : "11-0243", locker_password: "9009"},
+        ]
+    })
+
+    map.set("3XL", {
+        quantity : 6,
+        lockers : [
+            { locker_no : "11-0242", locker_password: "7522"},
+        ]
+    })
+
+    map.set("4XL", {
+        quantity : 3,
+        lockers : [
+            { locker_no : "11-0241", locker_password: "6563"},
+        ]
+    })
+
+    const supabase = getSupabaseClient();
+
+    if (supabase) {
+        let {data, error} = await supabase
+            .from('ppil_scp_shirt')
+            .select('quantity')
+            .eq('size', shirt_size)
+
+        const selectedSizeQuantity = data[0].quantity;
+        console.log("data ", selectedSizeQuantity);
+        console.log(`selected shirt size is: ${JSON.stringify(shirt_size)} and stock level is ${selectedSizeQuantity}`);
+
+        const mapEntry = map.get(shirt_size);
+        if (mapEntry) {
+            let selectedLocker;
+            if (mapEntry.lockers.length > 1) {
+                const lockerIndex = selectedSizeQuantity / mapEntry.quantity > 1 ? 0 : 1;
+                selectedLocker = mapEntry.lockers[lockerIndex];
+            } else {
+                selectedLocker = mapEntry.lockers[0];
+            }
+            console.log(`Assigned locker is: ${selectedLocker.locker_no}, password: ${selectedLocker.locker_password}`);
+            return selectedLocker;
+        } else {
+            console.log("No entry found in map for the selected shirt size.");
+            return null;
+        }
+    }
+
+}
 export async function POST(request: Request) {
 
     const supabase = getSupabaseClient();
@@ -30,9 +125,13 @@ export async function POST(request: Request) {
 
         // return Response.json({ name, email })
 
+        const locker = await get_locker(body.shirtSize)
+
+        console.log("locker ", locker)
+
         let response = {
-            'locker': 123,
-            'pass': 456
+            'locker': locker.locker_no,
+            'pass': locker.locker_password
         }
         return new Response(JSON.stringify(response), {
             status: 200,
